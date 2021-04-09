50fa354a83a3997f0e4dd3e567b3510dcb37a8e4
Tangnans <2220700182@qq.com>
2021-04-09 10:59:13 +0800

    0409

---
+++ src/com/xcq/test/Test.java
@@ -0,0 +1,14 @@
+package com.xcq.test;
+
+public class Test {
+    public static void main(String[] args) {
+//        String a = "abaaba";
+//        System.out.println(a.lastIndexOf("a"));
+       String str  = "aba-aba-aba-sin-jum-rap-bal";
+         String cxk[] = str.split("-",5);
+        for(String s:cxk){
+            System.out.println();
+          System.out.println(s);
+      }
+    }
+}
